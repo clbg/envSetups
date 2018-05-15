5b9302af14e364dae1a7c180048275fc7ee1824b
pengcheng <pengcheng_handsome@yeah.net>
2018-05-15 12:51:35 +0800

    combine two hexo setups

--- myscripts/setuphexo.sh
+++ myscripts/setuphexo.sh
@@ -1,10 +1,28 @@
-sudo apt-get install nodejs
-sudo apt-get install npm
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
 sudo apt-get install -y nodejs-legacy 
 npm -v
 sudo npm install -g hexo-cli
 
-
 hexo init ~/blog
 hexo init ~/techblog
 
+echo "SOURCE BLOG PATH:"
+echo  $BLOG
+
+echo "SOURCE TECH BLOG PATH:"
+echo $TECH_BLOG
+
+blogdst=~/blog
+techblogdst=~/techblog
+
+override="scaffolds source themes _config.yml package.json package-lock.json"
+
+for x in $override
+  do
+  rm -r $blogdst/$x
+  ln -s $BLOG/$x $blogdst
+
+  rm -r $techblogdst/$x
+  ln -s $TECH_BLOG/$x $techblogdst
+  done

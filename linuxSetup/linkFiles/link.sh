a38dcac54cbba74421912bed276c3d8d06d738f3
pengcheng <pengcheng_handsome@yeah.net>
2018-10-27 00:55:52 +0800

    fix link file : create folder before link

--- linuxSetup/linkFiles/link.sh
+++ linuxSetup/linkFiles/link.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+echo "linking .ssh/confg file"
+mkdir -p $HOME/.ssh
 ln -sf $HOME/envSetups/linuxSetup/link_files/HOME-.ssh-config $HOME/.ssh/config
+ 
+echo "linking .zshrc file"
 ln -sf $HOME/envSetups/linuxSetup/link_files/HOME-.zshrc $HOME/.zshrc
 mkdir -p $HOME/.config/pip
+
+echo "linking .config/pip/pip.conf"
 ln -sf $HOME/envSetups/linuxSetup/link_files/HOME-.config-pip-pip.conf $HOME/.config/pip/pip.conf

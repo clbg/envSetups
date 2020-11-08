c106474b5c5f94cac32c200c8208a191d3d58145
peng cheng <i@chengpeng.space>
2020-11-08 12:12:07 +0800

    fix setup_zsh task

--- py-installer/tasks/setup_zsh.py
+++ py-installer/tasks/setup_zsh.py
@@ -13,6 +13,7 @@ def setup_zsh():
 
 def install_oh_my_zsh():
     run_bash('git -C ~  clone https://github.com/ohmyzsh/ohmyzsh.git')
+    run_bash('bash ~/ohmyzsh/tools/install.sh')
 
 
 def link_zsh_files():

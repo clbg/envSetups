c978b5a8a8f81f31009b8e49ec6a2f56e48f91ba
charlie peng <spam@chengpeng.space>
2020-12-12 16:00:04 +0800

    add nodejs npm

--- py-installer/tasks/install_package.py
+++ py-installer/tasks/install_package.py
@@ -2,7 +2,7 @@ from ..utils.package_manager import PackageManager, update_source
 from ..utils.run_bash import run_bash_as_sudo, run_bash
 from ..utils.color_log import log
 
-COMMON_PACKAGES_TO_INSTALL = "tmux vim git zsh curl wget mosh htop rsync neovim"
+COMMON_PACKAGES_TO_INSTALL = "tmux vim git zsh curl wget mosh htop rsync neovim nodejs npm"
 PM_PKGLIST_DICT = {
     PackageManager.Pacman: f'{COMMON_PACKAGES_TO_INSTALL} openssh autojump python-pynvim',
     PackageManager.Apt: f'{COMMON_PACKAGES_TO_INSTALL} openssh-server autojump python3-neovim',

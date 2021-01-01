81d9689dfbfdd31c8617920caf1cd053160f3117
charlie <spam@chengpeng.space>
2021-01-01 22:40:03 +0800

    install homebrew in shell, and use bundle file instead of 'brew install'

--- py-installer/tasks/install_package.py
+++ py-installer/tasks/install_package.py
@@ -9,10 +9,12 @@ PM_PKGLIST_DICT = {
     PackageManager.Pacman: f'{COMMON_PACKAGES_TO_INSTALL} openssh autojump python-pynvim the_silver_searcher',
     PackageManager.Apt: f'{COMMON_PACKAGES_TO_INSTALL} openssh-server autojump python3-neovim silversearcher-ag',
     PackageManager.Yum: f'{COMMON_PACKAGES_TO_INSTALL} openssh autojump-zsh python36-neovim the_silver_searcher',
-    PackageManager.Brew: f'{COMMON_PACKAGES_TO_INSTALL} the_silver_searcher'
+    PackageManager.Brew: 'placeholder string, will use brew bundle instead'
 }
 
 PIP_PKG_LIST = 'icdiff'
+
+BREW_BUNDLE_FILE = '~/envSetups/macosSetup/configFiles/BrewFile'
 
 def install_packages(dist:Distribution, pkg_m: PackageManager):
     pkg_list = PM_PKGLIST_DICT[pkg_m]
@@ -32,7 +34,18 @@ def install_packages_with_package_manager(package_list: str, pkg_m: PackageManag
     if pkg_m == PackageManager.Yum:
         run_bash_as_sudo(f'yum -y install {package_list}')
     if pkg_m == PackageManager.Brew:
-        run_bash(f'brew install {package_list}')
+        #Install brew if not exist
+        if run_bash('command -v brew')!=0:
+            log('installing hoembrew...')
+            install_homebrew()
+            log('installing hoembrew done...')
+        #Generate BrewFile:
+        # brew bundle dump --describe --force --file="~/envSetups/macosSetup/configFiles/Brewfile"
+
+        #restore from bundle
+        log('restoring from BrewFile')
+        run_bash(f'brew bundle --file=\'{BREW_BUNDLE_FILE}\'')
+        log('restoring from BrewFile done')
     # todo choco
 
 def install_pacakges_with_pip(package_list:str, pkg_m: PackageManager):
@@ -47,3 +60,6 @@ def install_fzf_from_git():
     log('installing fzf')
     run_bash('git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf')
     run_bash('~/.fzf/install')
+
+def install_homebrew():
+    run_bash('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')

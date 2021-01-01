2861c2381e80e860874f69f4d59d3cd4f744928f
charlie <spam@chengpeng.space>
2021-01-02 00:00:02 +0800

    install karabiner

--- py-installer/install.py
+++ py-installer/install.py
@@ -2,9 +2,9 @@ from .tasks.install_package import install_packages
 from .tasks.setup_ssh import setup_ssh
 from .tasks.setup_zsh import setup_zsh
 from .tasks.setup_vim import setup_vim
-from .utils.running_platform import get_distribution
+from .tasks.setup_macos import setup_macos
+from .utils.running_platform import get_distribution,Distribution
 from .utils.package_manager import get_package_manager
-
 
 dist = get_distribution()
 pkg_m = get_package_manager(dist)
@@ -13,3 +13,6 @@ install_packages(dist, pkg_m)
 setup_ssh()
 setup_zsh()
 setup_vim(dist)
+
+if dist== Distribution.MacOS:
+    setup_macos()
---
+++ py-installer/tasks/setup_macos.py
@@ -0,0 +1,26 @@
+from ..utils.run_bash import run_bash, run_bash_as_sudo
+from ..utils.soft_link import soft_link
+from ..utils.running_platform import Distribution
+from ..utils.color_log import log
+
+def setup_macos():
+    setup_karabiner()
+
+def setup_karabiner():
+    log('setuping .confg for karabiner')
+    soft_link('~/envSetups/macosSetup/linkFiles/HOME-.config-karabiner','~/.config/karabiner')
+
+#def setup_vimrc():
+#    run_bash('mkdir -p ~/.config/nvim')
+#    run_bash('mkdir -p ~/.vim/undodir')
+#    soft_link('~/envSetups/linuxSetup/linkFiles/HOME-.vimrc','~/.vimrc')
+#    soft_link('~/.vimrc','~/.config/nvim/init.vim')
+#    
+#def update_vim_plug():
+#    # https://github.com/junegunn/vim-plug/issues/225
+#    #for vim
+#    run_bash(r"vim -E -s -u '~/.vimrc'  +PlugInstall +qall")
+#    #for nvim
+#    # TODO
+#    run_bash('nvim --headless +PlugInstall +qall')
+#
